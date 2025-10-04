e25e36f1d5b6b2cd936ae1f2cbab7ea0687d8e9a
Nisarg Shah <shahnisarg96@gmail.com>
2025-10-04 16:30:00 +0530

    add logger wrapper

---
+++ packages/common/src/logger.ts
@@ -0,0 +1,21 @@
+import winston from 'winston';
+
+const logLevel = process.env.LOG_LEVEL || 'info';
+
+export const logger = winston.createLogger({
+  level: logLevel,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
+
+export default logger;
