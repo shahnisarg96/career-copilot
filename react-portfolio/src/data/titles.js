e65ddb71b4cbaced4ebd034a9718225e0427dd4e
Nisarg Shah <shahnisarg96@gmail.com>
2025-12-03 12:00:00 +0530

    seed titles + skills presets

---
+++ react-portfolio/src/data/titles.js
@@ -0,0 +1,12 @@
+const titles = [
+    'a Software Engineer',
+    'a Full Stack Developer',
+    'a MEAN Stack Developer',
+    'a MERN Stack Developer',
+    'a Cloud Native Engineer',
+    'a Tech Lead',
+    'an Aspiring Solution Architect',
+    'an AI Explorer'
+];
+
+export default titles;
