ae80e7de77744c7ae61945d8861397d8efe1f5aa
Nisarg Shah <shahnisarg96@gmail.com>
2025-12-04 17:00:00 +0530

    add education/certificates sample data

---
+++ react-portfolio/src/data/certificates.js
@@ -0,0 +1,52 @@
+const certificates = [
+    {
+        title: 'BFL Appreciation',
+        imgSrc: './img/certificates/bfl_appreciation.jpg',
+    },
+    {
+        title: 'AZ-204 Microsoft Certified',
+        imgSrc: './img/certificates/AZ_204.png',
+    },
+    {
+        title: 'Namaste NodeJS',
+        imgSrc: './img/certificates/namaste_node.png',
+    },
+    {
+        title: 'Namaste React',
+        imgSrc: './img/certificates/namaste_react.png',
+    },
+    {
+        title: 'Namaste Javascript',
+        imgSrc: './img/certificates/namaste_js.png',
+    },
+    {
+        title: 'Cygnet Appreciation',
+        imgSrc: './img/certificates/cygnet_appreciation.png',
+    },
+    {
+        title: 'HackerRank',
+        imgSrc: './img/certificates/hackerrank.png',
+    },
+    {
+        title: 'Javascript Algorithms and DS',
+        imgSrc: './img/certificates/javascript.png',
+    },
+    {
+        title: 'Makerfest',
+        imgSrc: './img/certificates/makerfest.png',
+    },
+    {
+        title: 'Girlscript India Summit Volunteer',
+        imgSrc: './img/certificates/girlscript.png',
+    },
+    {
+        title: 'Way To Web Internship',
+        imgSrc: './img/certificates/waytoweb.png',
+    },
+    {
+        title: 'Invent-o-fest',
+        imgSrc: './img/certificates/invent-o-fest.png',
+    },
+];
+
+export default certificates;
