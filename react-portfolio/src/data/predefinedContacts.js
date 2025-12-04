345b6edeeda0940a41409ca04d9923b411ca0d7f
Nisarg Shah <shahnisarg96@gmail.com>
2025-12-04 18:30:00 +0530

    contact presets

---
+++ react-portfolio/src/data/predefinedContacts.js
@@ -0,0 +1,214 @@
+// Predefined contact methods with icons and labels
+export const predefinedContacts = [
+    // Email
+    {
+        label: 'Email',
+        icon: 'FiMail',
+        title: 'Send me an email',
+        hrefPrefix: 'mailto:',
+        placeholder: 'your.email@example.com',
+        type: 'email'
+    },
+
+    // Phone
+    {
+        label: 'Phone',
+        icon: 'FiPhone',
+        title: 'Call me',
+        hrefPrefix: 'tel:',
+        placeholder: '+1234567890',
+        type: 'phone'
+    },
+
+    // Social Media
+    {
+        label: 'LinkedIn',
+        icon: 'FaLinkedin',
+        title: 'Connect on LinkedIn',
+        hrefPrefix: 'https://linkedin.com/in/',
+        placeholder: 'your-profile',
+        type: 'social'
+    },
+    {
+        label: 'GitHub',
+        icon: 'FaGithub',
+        title: 'View my GitHub profile',
+        hrefPrefix: 'https://github.com/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'Twitter / X',
+        icon: 'FaTwitter',
+        title: 'Follow me on Twitter',
+        hrefPrefix: 'https://twitter.com/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'Instagram',
+        icon: 'FaInstagram',
+        title: 'Follow me on Instagram',
+        hrefPrefix: 'https://instagram.com/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'Facebook',
+        icon: 'FaFacebook',
+        title: 'Connect on Facebook',
+        hrefPrefix: 'https://facebook.com/',
+        placeholder: 'profile',
+        type: 'social'
+    },
+    {
+        label: 'YouTube',
+        icon: 'FaYoutube',
+        title: 'Subscribe to my channel',
+        hrefPrefix: 'https://youtube.com/@',
+        placeholder: 'channel',
+        type: 'social'
+    },
+    {
+        label: 'Twitch',
+        icon: 'FaTwitch',
+        title: 'Follow me on Twitch',
+        hrefPrefix: 'https://twitch.tv/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'TikTok',
+        icon: 'FaTiktok',
+        title: 'Follow me on TikTok',
+        hrefPrefix: 'https://tiktok.com/@',
+        placeholder: 'username',
+        type: 'social'
+    },
+
+    // Professional Networks
+    {
+        label: 'Portfolio Website',
+        icon: 'FiGlobe',
+        title: 'Visit my website',
+        hrefPrefix: 'https://',
+        placeholder: 'yourwebsite.com',
+        type: 'website'
+    },
+    {
+        label: 'Blog / Medium',
+        icon: 'FaMedium',
+        title: 'Read my blog',
+        hrefPrefix: 'https://medium.com/@',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'Dev.to',
+        icon: 'FaDev',
+        title: 'Follow me on Dev.to',
+        hrefPrefix: 'https://dev.to/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'Stack Overflow',
+        icon: 'FaStackOverflow',
+        title: 'View my Stack Overflow profile',
+        hrefPrefix: 'https://stackoverflow.com/users/',
+        placeholder: 'user-id/username',
+        type: 'social'
+    },
+    {
+        label: 'Dribbble',
+        icon: 'FaDribbble',
+        title: 'View my Dribbble portfolio',
+        hrefPrefix: 'https://dribbble.com/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'Behance',
+        icon: 'FaBehance',
+        title: 'View my Behance portfolio',
+        hrefPrefix: 'https://behance.net/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'CodePen',
+        icon: 'FaCodepen',
+        title: 'View my CodePen',
+        hrefPrefix: 'https://codepen.io/',
+        placeholder: 'username',
+        type: 'social'
+    },
+
+    // Communication Platforms
+    {
+        label: 'Discord',
+        icon: 'FaDiscord',
+        title: 'Connect on Discord',
+        hrefPrefix: 'https://discord.gg/',
+        placeholder: 'invite-code',
+        type: 'social'
+    },
+    {
+        label: 'Slack',
+        icon: 'FaSlack',
+        title: 'Message me on Slack',
+        hrefPrefix: 'https://',
+        placeholder: 'workspace.slack.com',
+        type: 'social'
+    },
+    {
+        label: 'Telegram',
+        icon: 'FaTelegram',
+        title: 'Message me on Telegram',
+        hrefPrefix: 'https://t.me/',
+        placeholder: 'username',
+        type: 'social'
+    },
+    {
+        label: 'WhatsApp',
+        icon: 'FaWhatsapp',
+        title: 'Message me on WhatsApp',
+        hrefPrefix: 'https://wa.me/',
+        placeholder: '1234567890',
+        type: 'phone'
+    },
+    {
+        label: 'Microsoft Teams',
+        icon: 'BsMicrosoftTeams',
+        title: 'Connect on Teams',
+        hrefPrefix: 'mailto:',
+        placeholder: 'your.email@example.com',
+        type: 'email'
+    },
+
+    // Other
+    {
+        label: 'Calendly',
+        icon: 'FiCalendar',
+        title: 'Schedule a meeting',
+        hrefPrefix: 'https://calendly.com/',
+        placeholder: 'username',
+        type: 'website'
+    },
+    {
+        label: 'Resume / CV',
+        icon: 'FiFileText',
+        title: 'Download my resume',
+        hrefPrefix: '',
+        placeholder: '/path/to/resume.pdf',
+        type: 'file'
+    },
+    {
+        label: 'Location',
+        icon: 'FiMapPin',
+        title: 'View location',
+        hrefPrefix: 'https://maps.google.com/?q=',
+        placeholder: 'City, Country',
+        type: 'location'
+    },
+];
